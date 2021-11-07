c73dbe3b888e1f8ed708ec282b0f9d66a0d7957b
pbl.gllgs <pbl.gllgs@gmail.com>
2021-11-07 02:24:13 -0300

    updates

--- index.js
+++ index.js
@@ -16,31 +16,7 @@ dbConnection();
 app.use(express.static('public'));
 
 //cors
-/* 
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-      res.send(200);
-    }
-    else {
-      next();
-    }
-};
- */
-
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-};
-
-app.use(cors('allowCrossDomain'));
+app.use(cors());
 
 //lectura y parseo del body
 app.use(express.json());
