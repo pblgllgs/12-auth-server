ebce0f9e42d00dbf0f53a5d708800101a16268df
pbl.gllgs <pbl.gllgs@gmail.com>
2021-11-07 14:22:43 -0300

    url whitelist 8

--- index.js
+++ index.js
@@ -23,8 +23,13 @@ app.use( express.json() );
 // Rutas
 app.use( '/api/auth', require('./routes/auth') );
 
+//manejar las demas rutas
+app.get('*', (req,res) => {
+    res.sendFile( path.resolve( __dirname, 'public/index.html'));
+});
 
+/* puerto que va a escuchar, depende del host,
+pero este es un mensaje cuando se despliega */
 app.listen( process.env.PORT, () => {
     console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
 });
-
