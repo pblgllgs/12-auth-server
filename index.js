1112a91e452e48179f2db0d606119c818fe63a10
pbl.gllgs <pbl.gllgs@gmail.com>
2021-11-07 13:43:47 -0300

    url whitelist 5

--- index.js
+++ index.js
@@ -16,24 +16,7 @@ dbConnection();
 app.use(express.static('public'));
 
 //cors
-const whitelist = ['http://localhost:4000']; // list of allow domain
-
-const corsOptions = {
-   origin: function(origin, callback) {
-      if (!origin) {
-         return callback(null, true);
-      }
-
-      if (whitelist.indexOf(origin) === -1) {
-         var msg = 'The CORS policy for this site does not ' +
-            'allow access from the specified Origin.';
-         return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-   }
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 //lectura y parseo del body
 app.use(express.json());
